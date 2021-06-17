484228b90b099fb4ce0b46ea5c14283095e8e0c7
Holland Pleskac <hollandpleskac@gmail.com>
2021-06-17 13:52:46 -0700

    new frontend

--- pages/index.js
+++ pages/index.js
@@ -1,101 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import Web3 from "web3";
+// got address and abi from deploying contract Lottery.sol in Lottery Project Ethereum Udemy Course --> deploy.js
+// const address = '0xBe57df6d9cAf41a41827F2294D46380954bB58e8'
+// const abi = '[{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]'
+
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon } from '@fortawesome/free-solid-svg-icons'
 
 const HomePage = () => {
-  const [manager, setManager] = useState('')
-  const [players, setPlayers] = useState([])
-  const [balance, setBalance] = useState('')
-  const [web3, setWeb3] = useState({})
-  const [lottery, setLottery] = useState({})
-  const [value, setValue] = useState(0)
-  const [feedback, setFeedback] = useState('')
-  const [loading, setLoading] = useState(true)
-
-  // got address and abi from deploying contract Lottery.sol in Lottery Project Ethereum Udemy Course --> deploy.js
-  const address = '0xBe57df6d9cAf41a41827F2294D46380954bB58e8'
-  const abi = '[{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]'
-
-  const connectToMetaMaskHandler = async () => {
-    // connect to meta mask
-    await window.ethereum.request({ method: "eth_requestAccounts" });
-    const web3 = new Web3(window.ethereum);
-    console.log(web3)
-    setWeb3(web3)
-
-    // make local contract instance
-    const lottery = new web3.eth.Contract(JSON.parse(abi), address)
-    console.log(lottery)
-    setLottery(lottery)
-
-    const manager = await lottery.methods.manager().call() // called on first metamask account (dont need a from property in call() fn)
-    const players = await lottery.methods.getPlayers().call()
-    const balance = await web3.eth.getBalance(lottery.options.address)
-
-    setManager(manager)
-    setPlayers(players)
-    setBalance(balance)
-    setLoading(false)
-  }
-
-  const onSubmitHandler = async (e) => {
-    console.log('entering into contract')
-    e.preventDefault()
-    if (Object.keys(lottery).length !== 0) {
-      // enter into contract
-      const accounts = await web3.eth.getAccounts()
-      setFeedback('Waiting on transaction success...')
-      await lottery.methods.enter().send({ // 15 to 30 seconds to process
-        from: accounts[0],
-        value: web3.utils.toWei(value, 'ether')
-      })
-      setFeedback('You have been entered!')
-    }
-  }
-
-  const pickWinnerHandler = async (e) => {
-    console.log('picking a winner')
-    if (Object.keys(lottery).length !== 0) {
-      const accounts = await web3.eth.getAccounts()
-      setFeedback('Waiting on transaction success...')
-      await lottery.methods.pickWinner().send({ from: accounts[0] })
-      setFeedback('A winner has been picked!')
-    }
-  }
-
-
-
   return (
-    <div className='flex flex-col h-screen justify-center items-center' >
-      {loading && <button className='px-4 py-3 bg-blue-600 text-white rounded' onClick={connectToMetaMaskHandler} >Connect to Metamask</button>}
-      {!loading &&
-        <div>
-          <h2 className='font-bold text-lg mb-1' >Lottery Contract</h2>
-          <p>
-            This contract is managed by {manager}.
-            There are currently {players.length} people entered,
-            competing to win {Object.keys(web3).length === 0 ? '' : web3.utils.fromWei(balance)} ether!
-          </p>
-          <hr />
-          <form onSubmit={onSubmitHandler} >
-            <h4 className='font-bold' >Want to try your luck?</h4>
-            <div>
-              <label>Amount of ether to enter</label>
-              <input
-                type="text"
-                value={value}
-                onChange={e => setValue(e.target.value)}
-                className='border-2 border-gray-800 ml-2'
-              />
-            </div>
-            <button className='border-2 border-gray-800 px-4' >Enter</button>
-          </form>
-          <hr />
-          <h4 className='font-bold' >Ready to pick a winner?</h4>
-          <button className='border-2 border-gray-800 px-4' onClick={pickWinnerHandler} >Pick a winner!</button>
-          <hr />
-          <h1>{feedback}</h1>
+    <div className='flex flex-col h-screen' >
+      <div className='flex justify-between items-center py-5 px-10 bg-white shadow'>
+        <div className='flex items-center' >
+          <img src="/mountain-logo.png" alt="Logo" width='48' />
+          <h1 className='text-gray-800 text-xl pl-4' >Lottery</h1>
         </div>
-      }
+        <div>
+          <button className='px-4 mr-4 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50 transition ease-in duraiton-100' style={{ height: 40 }} >
+            Connect to MetaMask
+          </button>
+          <button className='bg-gray-700 rounded-lg hover:bg-gray-800 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50  transition ease-in duration-100' style={{ width: 40, height: 40 }} >
+            <FontAwesomeIcon icon={faMoon} className='text-white' />
+          </button>
+        </div>
+      </div>
+      <div className='flex-grow flex flex-col justify-center items-center' >
+        <p className='text-gray-600 text-sm mb-2' >&nbsp;</p>
+        <h1 className='text-5xl text-blue-600 ' >Want to try your luck?</h1>
+        <p className='mt-4 text-gray-600' >0 people entered, competing to win 0 ether</p>
+        <div className='flex items-center mt-9' >
+          <input type="text" placeholder='Amount in Ether' className='px-4 border-2 border-gray-500 rounded-l-lg focus:outline-none focus:border-gray-600 transition ease-in duration-100' style={{ width: 400, height: 50 }} />
+          <button className='px-2 border-2 border-blue-600 text-white bg-blue-600 rounded-r-lg hover:bg-blue-700 focus:outline-none focus:border-blue-700 focus:bg-blue-700 transition ease-in duration-100' style={{ height: 50 }} >Enter</button>
+        </div>
+        <p className='text-gray-600 text-sm mt-2' >&nbsp;</p>
+      </div>
+
     </div>
   )
 }
