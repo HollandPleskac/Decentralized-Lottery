a021d6fd7a76b81d7e6c3091f48b506f003323a6
Holland Pleskac <hollandpleskac@gmail.com>
2021-06-23 15:40:08 -0700

    updated contract

--- ethereum/Lottery.sol
+++ ethereum/Lottery.sol
@@ -1,12 +1,35 @@
-pragma solidity ^0.8.4;
+pragma solidity 0.6.6;
 
-contract Lottery {
+import "https://github.com/smartcontractkit/chainlink/blob/develop/evm-contracts/src/v0.6/VRFConsumerBase.sol";
+
+contract Lottery is VRFConsumerBase {
     
     address public manager;
     address[] public players;
     
-    constructor() {
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    uint256 public randomResult;
+    
+    constructor()
+        VRFConsumerBase(
+            0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B, // VRF Coordinator
+            0x01BE23585060835E02B77ef475b0Cc51aA1e0709  // LINK Token
+        ) public
+    {
         manager = msg.sender;
+        keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
+        fee = 0.1 * 10 ** 18; // 0.1 LINK (Varies by network)
+    }
+    
+    function getRandomNumber() public returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+        return requestRandomness(keyHash, fee);
+    }
+    
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        // randomResult = randomness.mod(players.length-1); // numbers from 0 to players.length-1
+        randomResult = randomness.mod(20).add(1);
     }
     
     function enter() public payable {
@@ -22,6 +45,7 @@ contract Lottery {
     }
     
     function pickWinner() public payable restricted {
+        // uint index = randomResult;
         uint index = random() % players.length;
         payable(players[index]).transfer(address(this).balance); // this.balance is the amount of money in the contract
         players = new address[](0); // brand new array of addresses - (0) means the array has an inital size of 0
